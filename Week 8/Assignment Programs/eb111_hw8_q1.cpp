9795391e5b933e6fe402f01f5760efeeeb23e34b
Everett <everett.bowline@gmail.com>
2023-08-29 11:12:53 -0400

    Modified indexLocations variables

--- Week 8/Assignment Programs/eb111_hw8_q1.cpp
+++ Week 8/Assignment Programs/eb111_hw8_q1.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int minInArray(int arr[3], int arrSize);
-int minimumFinder(int min, int indexLocation, int& temp);
-int indexLocations(int temp, int num, int indexLocation);
+void indexLocations(int arr[20], int num, int indexLocation);
 
 const int ARRAYSIZE = 20;
 
@@ -13,7 +12,6 @@ int main () {
     int arrSize = 5, i = 0, minInput = 0;
     int indices;
     int smol = 0, temp = 0;
-    int test1 = 45, test2 = 5, test3 = 5, test4 = 99, test5 = 5;
 
     cout<<"Please enter 20 integers separated by a space:"<<endl;
     
@@ -22,17 +20,9 @@ int main () {
         i++;
     }
 
-    // smol = temp = arr[0];
-
-    // for ( i = 0; i < 5; i++ ) {
-    //     if ( smol > arr[i] ) {
-    //         smol = minimumFinder(arr[i], i, temp);
-    //     }
-    // }
-
     minInput = minInArray(arr, arrSize);
 
-    cout<<"The minimum value is "<<minInput<<", and it is located in the following indices: "<<smol<<endl;
+    cout<<"The minimum value is "<<minInput<<", and it is located in the following indices:"<<indexLocations(arr, minInput, smol)<<endl;
 
 }
 
@@ -42,33 +32,33 @@ int minInArray(int arr[], int arrSize) {
     for ( i = 0; i < arrSize; i++ ) {
         if ( i == 0 ) {
             smallArr = arr[i];
-            // str = to_string(i);
-            // minimumFinder(smallArr, str);
         }
         else if ( arr[i] < arr[i-1] ) {
             smallArr = arr[i];
-            // str = to_string(i);
-            // minimumFinder(smallArr, str);
         }
     }
 
     return smallArr;
 }
 
-int minimumFinder(int min, int indexLocation, int& temp) {
+// int minimumFinder(int min, int indexLocation, int& temp) {
 
-    if ( indexLocation == 0 ) {
-        temp = min;
-        cout<<temp<<endl;
-    }
-    else if ( min < temp ) {
-        temp = min;
-    }
+//     if ( indexLocation == 0 ) {
+//         temp = min;
+//         cout<<temp<<endl;
+//     }
+//     else if ( min < temp ) {
+//         temp = min;
+//     }
 
-    return temp;
-}
-
-// int indexLocations(int num, int indexLocation) {
-
-
+//     return temp;
 // }
+
+void indexLocations(int arr[], int num, int indexLocation) {
+
+    for ( int i = 0; i < 5; i++ ) {
+        if ( arr[i] == num ) {
+            cout<<" "<<i;
+        }
+    }
+}
