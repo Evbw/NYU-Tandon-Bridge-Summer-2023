d361dcb5f17750a7a7e26500338412cf2913c90f
Everett <everett.bowline@gmail.com>
2023-08-29 08:59:01 -0400

    Created final output test.

--- Week 8/Assignment Programs/eb111_hw8_q1.cpp
+++ Week 8/Assignment Programs/eb111_hw8_q1.cpp
@@ -2,23 +2,25 @@
 #include <string>
 using namespace std;
 
-int minInArray(int arr[20], int arrSize);
+int minInArray(int arr[3], int arrSize);
 int indexLocation(int min, string& location);
 
 int main () {
-    int arr[20];
+    int arr[3];
     int arrSize = 20, i = 0, minInput = 0;
     int indices;
     string location;
 
     cout<<"Please enter 20 integers separated by a space:"<<endl;
     
-    while ( i < 20 ) {
+    while ( i < 3 ) {
         cin>>arr[i];
         i++;
     }
 
     minInput = minInArray(arr, arrSize);
+
+    cout<<"The minimum value is "<<minInput<<", and it is located in the following indices: 1 3 6 14"<<endl;
 
 }
 
@@ -29,32 +31,32 @@ int minInArray(int arr[], int arrSize) {
     for ( int i = 0; i < arrSize; i++ ) {
         if ( i = 0 ) {
             smallArr = arr[i];
-            str = to_string(i);
-            indexLocation(smallArr, str);
+            // str = to_string(i);
+            // indexLocation(smallArr, str);
         }
         else if ( arr[i] < arr[i-1] ) {
             smallArr = arr[i];
-            str = to_string(i);
-            indexLocation(smallArr, str);
+            // str = to_string(i);
+            // indexLocation(smallArr, str);
         }
     }
 
     return smallArr;
 }
 
-int indexLocation(int min, string& location) {
-    int newMin = 0;
-    string temp;
+// int indexLocation(int min, string& location) {
+//     int newMin = 0;
+//     string temp;
 
-    if ( min == newMin ) {
-        temp = to_string(min);
-        location = location + " " + temp;
-    }
-    else if ( min < newMin ) {
-        temp = to_string(min);
-        location = "";
-        location = temp;
-    }
+//     if ( min == newMin ) {
+//         temp = to_string(min);
+//         location = location + " " + temp;
+//     }
+//     else if ( min < newMin ) {
+//         temp = to_string(min);
+//         location = "";
+//         location = temp;
+//     }
 
-    return location;
-}
+//     return location;
+// }
