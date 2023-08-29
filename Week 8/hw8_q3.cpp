bd8404a190d501d0e82bda5b72dc7a2871e3422b
Everett <everett.bowline@gmail.com>
2023-08-29 16:00:52 -0400

    Realized I built an almost perfect splitParity function

--- Week 8/hw8_q3.cpp
+++ Week 8/hw8_q3.cpp
@@ -11,8 +11,8 @@ int main () {
     int arr1[10] = {9, 2, 14, 12, -3};
     int arr1Size = 5;
 
-    int arr2[10] = {21, 12, 6, 7, 14};
-    int arr2Size = 5;
+    int arr2[10] = {21, 12, 6, 7, 14, 16, 32, 33, 88, 2};
+    int arr2Size = 10;
 
     int arr3[10] = {3, 6, 4, 1, 12};
     int arr3Size = 5;
@@ -50,25 +50,53 @@ void reverseArray(int arr[], int arrSize) {
 
 void removeOdd(int arr[], int& arrSize) {
     int temp, i;
+    int size = arrSize;
 
-    for ( i = 0 ; i < arrSize ; i++ ) {
-        if ( arr[i] % 2 == 1 && i < arrSize/2 ) {
-            cout<<"i "<<i<<endl;
-            cout<<"arr[i] "<<arr[i]<<endl;
-            temp = arr[i];
-            arr[i] = arr[arrSize - ( 1 + i)];
-            cout<<"arr[i]2 "<<arr[i]<<endl;
-            cout<<"arr[arrSize - ( 1 + i)] "<<arr[arrSize - ( 1 + i)]<<endl;
-            arr[arrSize - ( 1 + i)] = temp;
-            cout<<"arr[arrSize - ( 1 + i)]2 "<<arr[arrSize - ( 1 + i)]<<endl;
-            arrSize--;
+    for ( i = 0 ; i < size ; i++ ) {
+        if ( arr[size - 1] % 2 == 1 ) {
+            size--;
+            cout<<"size "<<size<<endl;
+            cout<<"arr["<<size<<"] "<<arr[size]<<endl;
         }
-        else if ( arr[i] % 2 == 1 ) {
-            arrSize--;
+        if ( arr[i] % 2 == 1  ) {
+            // && i < size/2
+            
+            cout<<"arr["<<i<<"] "<<arr[i]<<endl;
+            temp = arr[i];
+            cout<<"temp "<<temp<<endl;
+            arr[i] = arr[size - 1];
+            cout<<"arr["<<i<<"]2 "<<arr[i]<<endl;
+            cout<<"arr["<<size - 1<<"] "<<arr[size - 1]<<endl;
+            arr[size - 1] = temp;
+            cout<<"temp2 "<<temp<<endl;
+            cout<<"arr["<<size - 1<<"]2 "<<arr[size - 1]<<endl;
+            size--;
         }
     }
 }
 
 void splitParity(int arr[], int arrSize) {
+
+    // for ( i = 0 ; i < size ; i++ ) {
+    //     if ( arr[size - 1] % 2 == 1 ) {
+    //         size--;
+    //         cout<<"size "<<size<<endl;
+    //         cout<<"arr["<<size<<"] "<<arr[size]<<endl;
+    //     }
+    //     if ( arr[i] % 2 == 1  ) {
+    //         // && i < size/2
+            
+    //         cout<<"arr["<<i<<"] "<<arr[i]<<endl;
+    //         temp = arr[i];
+    //         cout<<"temp "<<temp<<endl;
+    //         arr[i] = arr[size - 1];
+    //         cout<<"arr["<<i<<"]2 "<<arr[i]<<endl;
+    //         cout<<"arr["<<size - 1<<"] "<<arr[size - 1]<<endl;
+    //         arr[size - 1] = temp;
+    //         cout<<"temp2 "<<temp<<endl;
+    //         cout<<"arr["<<size - 1<<"]2 "<<arr[size - 1]<<endl;
+    //         size--;
+    //     }
+    // }
 
 }
