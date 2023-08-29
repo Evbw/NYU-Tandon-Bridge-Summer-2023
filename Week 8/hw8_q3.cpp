c03f2a5672009d4597b95681731f02a9a4031cfa
Everett <everett.bowline@gmail.com>
2023-08-29 12:49:36 -0400

    Added file and declared functions

---
+++ Week 8/hw8_q3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverseArray(int arr[], int arrSize);
+void removeOdd(int arr[], int& arrSize);
+void splitParity(int arr[], int arrSize)''
+
+int main () {
+
+}
+
+void reverseArray(int arr[], int arrSize) {
+
+}
+
+void removeOdd(int arr[], int& arrSize) {
+
+}
+
+void splitParity(int arr[], int arrSize)
