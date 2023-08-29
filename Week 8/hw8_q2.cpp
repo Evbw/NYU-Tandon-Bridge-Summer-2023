8c77dfaf59f1b72f300869bd435f2c9e5be2ac29
Everett <everett.bowline@gmail.com>
2023-08-29 12:27:10 -0400

    Built out main function

--- Week 8/hw8_q2.cpp
+++ Week 8/hw8_q2.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+bool isPalindrome(string str);
+
 int main () {
+    string word;
+
+    cout<<"Please enter a word: ";
+    cin>>word;
+    
+    if (isPalindrome(word)) {
+
+        cout<<word<<" is a palindrome"<<endl;
+
+    }    
+
+}
+
+bool isPalindrome(string str) {
+
 }
