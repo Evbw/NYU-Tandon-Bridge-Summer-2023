19a85c5b4f18e39e2f499c258d1cb9906e34941c
Everett <everett.bowline@gmail.com>
2023-08-29 12:45:12 -0400

    Finalized main function and isPalindrome function

--- Week 8/hw8_q2.cpp
+++ Week 8/hw8_q2.cpp
@@ -2,30 +2,36 @@
 #include <string>
 using namespace std;
 
-void isPalindrome(string str);
+bool isPalindrome(string str);
 
 int main () {
     string word;
 
     cout<<"Please enter a word: ";
     cin>>word;
-    isPalindrome(word);
     
-    // if (isPalindrome(word)) {
-
-    //     cout<<word<<" is a palindrome"<<endl;
-
-    // }    
+    if ( isPalindrome(word) ) {
+        cout<<word<<" is a palindrome"<<endl;
+    }
+    else {
+        cout<<word<<" is not a palindrome"<<endl;
+    }
 
 }
 
-void isPalindrome(string str) {
+bool isPalindrome(string str) {
     int n, m;
     string newword;
 
     for ( n = str.length() - 1; n >= 0; n-- ) {
         newword += str[n];
     }
-    cout<<"new word "<<newword<<endl;
-    cout<<"str      "<<str<<endl;
+
+    if ( newword == str ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
 }
