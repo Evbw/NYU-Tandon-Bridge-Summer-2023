49e850cdcde07ded913fa6ef860a6e8ecbe2cb8a
Everett <everett.bowline@gmail.com>
2023-08-29 11:30:28 -0400

    Finished main function. Returns expected results

--- Week 8/Assignment Programs/eb111_hw8_q1.cpp
+++ Week 8/Assignment Programs/eb111_hw8_q1.cpp
@@ -7,59 +7,41 @@ int minInArray(int arr[ARRAYSIZE], int arrSize);
 void indexLocations(int arr[ARRAYSIZE], int num, int indexLocation);
 
 int main () {
-    int arr[5];
-    int arrSize = 5, i = 0, minInput = 0;
-    int indices;
-    int smol = 0, temp = 0;
+    int arr[ARRAYSIZE];
+    int arrSize = ARRAYSIZE, i = 0, minInput = 0;
+    int smol = 0;
 
     cout<<"Please enter 20 integers separated by a space:"<<endl;
     
-    while ( i < 5 ) {
+    while ( i < ARRAYSIZE ) {
         cin>>arr[i];
         i++;
     }
 
     minInput = minInArray(arr, arrSize);
 
-    
-
     cout<<"The minimum value is "<<minInput<<", and it is located in the following indices:";
     indexLocations(arr, minInput, smol);
     cout<<endl;
-
 }
 
 int minInArray(int arr[], int arrSize) {
-    int smallArr = 0, i = 0;
+    int smallArr = 0, i;
     
     for ( i = 0; i < arrSize; i++ ) {
         if ( i == 0 ) {
             smallArr = arr[i];
         }
-        else if ( arr[i] < arr[i-1] ) {
+        else if ( arr[i] < smallArr ) {
             smallArr = arr[i];
         }
     }
-
     return smallArr;
 }
 
-// int minimumFinder(int min, int indexLocation, int& temp) {
-
-//     if ( indexLocation == 0 ) {
-//         temp = min;
-//         cout<<temp<<endl;
-//     }
-//     else if ( min < temp ) {
-//         temp = min;
-//     }
-
-//     return temp;
-// }
-
 void indexLocations(int arr[], int num, int indexLocation) {
 
-    for ( int i = 0; i < 5; i++ ) {
+    for ( int i = 0; i < ARRAYSIZE; i++ ) {
         if ( arr[i] == num ) {
             cout<<" "<<i;
         }
