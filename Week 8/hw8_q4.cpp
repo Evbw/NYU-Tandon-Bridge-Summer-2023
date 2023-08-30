c2629329e9155180fea236f82ced34ae5e240a60
Everett <everett.bowline@gmail.com>
2023-08-30 11:06:03 -0400

    Added splitEntry function to separate numerals in the input

--- Week 8/hw8_q4.cpp
+++ Week 8/hw8_q4.cpp
@@ -5,12 +5,14 @@ const int PIN[5] = {2, 1, 1, 3, 5};
 
 void encryptedPIN (int arr[], int arrSize);
 int getRandom(int arr[], int arrSize);
+void splitEntry(int arr[], int num, int arrSize);
 
 int main () {
     int num[10];
     int digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int entry[5];
-    int numSize = 10, digitsSize = 10, entrySize = 5;    
+    int numSize = 10, digitsSize = 10, entrySize = 5;  
+    int input;  
     srand(time(0));
 
     num[10] = getRandom(num, numSize);
@@ -25,12 +27,16 @@ int main () {
     for ( int i = 0; i < numSize; i++ ) {
         cout<<num[i]<<'\t';
     }
+    cout<<endl;
+    cin>>input;
+
+    splitEntry(entry, input, entrySize);
 
     for ( int i = 0; i < entrySize; i++ ) {
-        cin>>entry[i];
+        cout<<entry[i]<<" ";
     }
 
-    encryptedPIN (entry, entrySize);
+    // encryptedPIN (entry, entrySize);
 
 }
 
@@ -39,6 +45,18 @@ int getRandom(int arr[], int arrSize) {
         arr[i] = ( rand() % 3 ) + 1;
     }
     return arr[10];
+}
+
+void splitEntry(int arr[], int num, int arrSize) {
+
+    for ( int i = arrSize - 1; i > 0 ; i-- ) {
+        arr[i] = num % 10;
+        num /= 10;
+        if ( num < 10 ) {
+            arr[i-1] = num;
+        }
+    }
+
 }
 
 void encryptedPIN (int arr[], int arrSize) {
