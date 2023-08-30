e8483043bc504c98d5f99ca54d5b02cd9b417d26
Everett <everett.bowline@gmail.com>
2023-08-30 09:31:18 -0400

    Began testing getRandom function

--- Week 8/hw8_q4.cpp
+++ Week 8/hw8_q4.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
 
-const int PIN = {2, 1, 1, 3, 5}
+const int PIN[5] = {2, 1, 1, 3, 5};
 
 void encryptedPIN (int arr[], int arrSize);
+int getRandom(int arr[], int arrSize);
 
 int main () {
     int num[10];
     int numSize = 10;
     srand(time(0));
 
-    for ( int i = 0; i < numSize; i++ ) {
-        num[i] = rand() % 4;
-    }
+    num[10] = getRandom(num, numSize);
 
+}
+
+int getRandom(int arr[], int arrSize) {
+    for ( int i = 0; i < arrSize; i++ ) {
+        arr[i] = rand() % 4;
+    }
+    return arr[10];
 }
