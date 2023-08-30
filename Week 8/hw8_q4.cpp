70371cf8f02326b3dbca100f3dd7f63c9c66982f
Everett <everett.bowline@gmail.com>
2023-08-30 09:41:28 -0400

    Added input number array and display

--- Week 8/hw8_q4.cpp
+++ Week 8/hw8_q4.cpp
@@ -8,13 +8,15 @@ int getRandom(int arr[], int arrSize);
 
 int main () {
     int num[10];
-    int numSize = 10;
+    int digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int numSize = 10, digitsSize = 10;
     srand(time(0));
 
     num[10] = getRandom(num, numSize);
 
-    for ( int i = 0; i < numSize; i++ ) {
-        cout<<num[i]<<"\t";
+    cout<<"PIN:\t";
+    for ( int i = 0; i < digitsSize; i++ ) {
+        cout<<digits[i]<<'\t';
     }
 
 }
