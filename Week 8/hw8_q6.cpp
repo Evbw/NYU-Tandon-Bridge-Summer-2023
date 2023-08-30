de7ae79b7bf0374b051eff57f80f764ade219397
Everett <everett.bowline@gmail.com>
2023-08-30 13:37:31 -0400

    Began testing. Mixed success

--- Week 8/hw8_q6.cpp
+++ Week 8/hw8_q6.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+    string line;
+
+    cout<<"Please enter a line of text: "<<endl;
+    getline(cin, line);
+
+    for ( int i = 0; i < ( line.length() - 1) ; i++ ) {
+        if ( 58 > line[i] && line[i] > 46 ) {
+            if ( line[i - 1] != 32 ) {
+                continue;
+            }
+            line[i] = 120;
+        }
+    }
     
-    
+    cout<<line;
 }
