9dc9bba26e04b2bb02ea24eb2c239db5a43a21c3
Everett <everett.bowline@gmail.com>
2023-08-30 09:26:47 -0400

    Added secondary array, array size and began adding loop to populate secondary array

--- Week 8/hw8_q4.cpp
+++ Week 8/hw8_q4.cpp
@@ -6,5 +6,12 @@ const int PIN = {2, 1, 1, 3, 5}
 void encryptedPIN (int arr[], int arrSize);
 
 int main () {
-    
+    int num[10];
+    int numSize = 10;
+    srand(time(0));
+
+    for ( int i = 0; i < numSize; i++ ) {
+        num[i] = rand() % 4;
+    }
+
 }
