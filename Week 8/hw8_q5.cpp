48fbd9ea3b4cdb2517d47ddae07d579a87724847
Everett <everett.bowline@gmail.com>
2023-08-30 13:13:16 -0400

    Instantiated file and began writing main function

---
+++ Week 8/hw8_q5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string first, middle, last;
+
+    cout<<"Please input your first name, middle name, and last name: "<<endl;
+    cin>>first>>middle>>last;
+
+    
+}
