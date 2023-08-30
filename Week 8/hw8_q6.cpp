c8623acc3fdf7a8925f560ea96106a41170047a1
Everett <everett.bowline@gmail.com>
2023-08-30 14:05:44 -0400

    Realized my method wouldn't work for words that have numbers at the front

--- Week 8/hw8_q6.cpp
+++ Week 8/hw8_q6.cpp
@@ -3,19 +3,18 @@
 using namespace std;
 
 int main() {
-    string line;
+    string line, word;
+    int j = 0;
 
     cout<<"Please enter a line of text: "<<endl;
     getline(cin, line);
 
-    for ( int i = 0; i < ( line.length() - 1) ; i++ ) {
-        if ( 58 > line[i] && line[i] > 46 ) {
-            if ( line[i - 1] != 32 ) {
-                continue;
-            }
-            line[i] = 120;
+    for ( int i = 0; i < ( line.length() - 1 ) ; i++ ) {
+        if ( line[i] == 32 ) {
+            word = line.substr(j, line[i-1] - j);
+            cout<<line.substr(j, line[i-1] - j)<<endl;
+            j = line[i] + 1;
+
         }
     }
-    
-    cout<<line;
 }
