aa1383f5be7442da37a4680e4cbef3e8cff8ebbd
Everett <everett.bowline@gmail.com>
2023-08-30 12:19:58 -0400

    Began testing encryptedPIN

--- Week 8/hw8_q4.cpp
+++ Week 8/hw8_q4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-const int PIN[5] = {2, 6, 1, 3, 5};
+const int PIN[5] = {2, 1, 1, 3, 5};
 const int PINSIZE = 5;
 
 void encryptedPIN (int arr[], int arr2[], int arr3[], int arrSize, int arrSize2, int arrSize3);
@@ -11,8 +11,8 @@ void splitEntry(int arr[], int num, int arrSize);
 int main () {
     int num[10];
     int digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int entry[5], PINcheck[5];
-    int numSize = 10, digitsSize = 10, entrySize = 5, PINcheck = 5;  
+    int entry[5];
+    int numSize = 10, digitsSize = 10, entrySize = 5;  
     int input;  
     srand(time(0));
 
@@ -31,10 +31,16 @@ int main () {
     cout<<endl;
     cin>>input;
 
+    if ( input > 99999 ) {
+        cout<<"Your PIN is not correct"<<endl;
+        return 0;
+    }
+
     splitEntry(entry, input, entrySize);
 
     encryptedPIN (num, digits, entry, numSize, digitsSize, entrySize);
 
+    return 0;
 }
 
 int getRandom(int arr[], int arrSize) {
@@ -58,18 +64,22 @@ void splitEntry(int arr[], int num, int arrSize) {
 
 void encryptedPIN (int arr[], int arr2[], int arr3[], int arrSize, int arrSize2, int arrSize3) {
     int pin = 0;
+    int PINcheck[5];
+    int PINchecksize = 5;
+
+
 
     for ( int i = 0; i < PINSIZE; i++ ) {
         for ( int j = 0; j < arrSize2; j++ ) {
-            if ( PIN[i] == arr2[j] ) {
+            if ( arr3[i] == arr2[j] ) {
                 pin = 10 * pin + PIN[i];
             }
         }
     }
 
-    splitEntry(PINcheck, int num, int arrSize)
-    
-    if (PIN == PINcheck) {
+    splitEntry(PINcheck, pin, PINchecksize);
+
+    if ( PIN[0] == PINcheck[0] && PIN[1] == PINcheck[1] && PIN[2] == PINcheck[2] && PIN[3] == PINcheck[3] && PIN[4] == PINcheck[4] ) {
         cout<<"Your PIN is correct"<<endl;
     }
     else {
