e35545301b20b3c69cb7df22c9f7a5a0ba8693a1
Everett <everett.bowline@gmail.com>
2023-09-03 16:28:57 -0400

    Simplified code

--- Week 9/hw9_q1.cpp
+++ Week 9/hw9_q1.cpp
@@ -4,55 +4,51 @@
 #include <vector>
 
 void wordSplitter (std::string str);
-bool compareFunction (std::string a, std::string b) {return a<b;}
 
 int main() {
-    //Initialized string variables, one for input and one for individual words after I split them apart
+    //Initialize string variables, one for input and one for individual words after I split them apart
     std::string line, word;
     
     //Request user input
     std::cout<<"Please enter a line of text: "<<std::endl;
     getline(std::cin, line);
-    //Take that line of text begin loop break it down into single words
+    //Take that line of text begin loop break it down into single words. Print out individual letters
     wordSplitter (line);
 }
 
 void wordSplitter (std::string str) {
-    //Initialized string variable for individual words after I split them apart
+    //Initialize string variable for individual words after I split them apart
     std::string word;
+    //Initialize vectors for the string
     std::vector<char> letters;
-    std::vector<char> lettercounter;
-    int wordcount = 1, lettercount = 1;
-    
-
+    //Initialize counters for the words and lettesr
+    int wordcount = 1, lettercount = 0;
+    //Populate letters vector based on input string
     for ( int i = 0 ; i <= str.length() ; i++ ) {
         letters.push_back(str[i]);
     }
-
+    //Being loop to isolate individual letters
     for ( int i = 0 ; i <= letters.size() ; i++ ) {
-        //Check if the element of the string is a space or the end of the line
-        
+        //Check if the element of the string is a space. If so, increase wordcount        
         if ( letters[i] == 32 ) {
             wordcount++;
         }
+        //If the letter is uppercase, make it lowercase
         if ( letters[i] >= 65 && letters[i] <= 90 ) {
             letters[i] = letters[i] + 32;
         }
-        if ( letters[i] >= 97 && letters[i] <= 122 ) {
-            lettercounter.push_back(letters[i]);
-        }
     }
-    std::sort(lettercounter.begin(), lettercounter.end());
+    std::sort(letters.begin(), letters.end());
 
     std::cout<<wordcount<<'\t'<<"words"<<std::endl;
 
-    for ( int i = 0 ; i <= lettercounter.size() ; i++ ) {
+    for ( int i = 0 ; i <= letters.size() ; i++ ) {
            
-        if ( lettercounter[i] != lettercounter[i + 1] ) {
-            if ( lettercounter[i] < 97 ) {
+        if ( letters[i] != letters[i + 1] ) {
+            if ( letters[i] < 97 ) {
                 continue;
             }
-            std::cout<<lettercount<<'\t'<<lettercounter[i]<<std::endl;
+            std::cout<<lettercount<<'\t'<<letters[i]<<std::endl;
             lettercount = 1;
         }
         else {
