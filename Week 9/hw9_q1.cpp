dff06a06198a41ae09c07e6e27b912040e8fb97e
Everett <everett.bowline@gmail.com>
2023-09-03 15:31:39 -0400

    Added the sort function and created loop to print the result

--- Week 9/hw9_q1.cpp
+++ Week 9/hw9_q1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
 
 void wordSplitter (std::string str);
+bool compareFunction (std::string a, std::string b) {return a<b;}
 
 int main() {
     //Initialized string variables, one for input and one for individual words after I split them apart
@@ -19,8 +21,8 @@ void wordSplitter (std::string str) {
     //Initialized string variable for individual words after I split them apart
     std::string word;
     std::vector<char> letters;
-    std::vector<char> lettercount;
-    int wordcount = 1;
+    std::vector<char> lettercounter;
+    int wordcount = 1, lettercount = 1;
     
 
     for ( int i = 0 ; i <= str.length() ; i++ ) {
@@ -33,25 +35,28 @@ void wordSplitter (std::string str) {
         if ( letters[i] == 32 ) {
             wordcount++;
         }
-        // if ( letters[i] < 65 || letters[i] > 122 ) {
-        //     letters.erase(letters.begin()+i);
-        // }
-        // else if ( letters[i] > 90 && letters[i] < 97 ) {
-        //     letters.erase(letters.begin()+i);
-        // }
         if ( letters[i] >= 65 && letters[i] <= 90 ) {
             letters[i] = letters[i] + 32;
         }
         if ( letters[i] >= 97 && letters[i] <= 122 ) {
-            lettercount.push_back(letters[i]);
+            lettercounter.push_back(letters[i]);
         }
     }
+    std::sort(lettercounter.begin(), lettercounter.end());
 
     std::cout<<wordcount<<'\t'<<"words"<<std::endl;
 
-    for ( int i = 0 ; i <= lettercount.size() ; i++ ) {
-        if ( lettercount[i] ) {
-
+    for ( int i = 0 ; i <= lettercounter.size() ; i++ ) {
+           
+        if ( lettercounter[i] != lettercounter[i + 1] ) {
+            if ( lettercounter[i] < 97 ) {
+                continue;
+            }
+            std::cout<<lettercount<<'\t'<<lettercounter[i]<<std::endl;
+            lettercount = 1;
+        }
+        else {
+            lettercount++;
         }
     }
     
