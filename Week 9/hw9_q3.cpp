3dbb8840bfd6363eb4a99874b41d6507ab4323d1
Everett <everett.bowline@gmail.com>
2023-09-04 13:03:54 -0400

    Declare and add required functions from document

---
+++ Week 9/hw9_q3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//Declare functions
+int* getPosNums1(int* arr, int arrSize, int& outPosArrSize);
+int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr);
+void getPosNums3(int* arr, int arrSize, int*& outPosArr, int& outPosArrSize);
+void getPosNums4(int* arr, int arrSize, int** outPosArrPtr, int* outPosArrSizePtr)
+
+int main() {
+    
+}
+
+int* getPosNums1(int* arr, int arrSize, int& outPosArrSize) {
+
+}
+
+int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr) {
+
+}
+
+void getPosNums3(int* arr, int arrSize, int*& outPosArr, int& outPosArrSize) {
+
+}
+
+void getPosNums4(int* arr, int arrSize, int** outPosArrPtr, int* outPosArrSizePtr) {
+
+}
