779cb879457fb22e9fdde3b3c47f85aeba32fc2f
Everett <everett.bowline@gmail.com>
2023-09-03 22:19:50 -0400

    Add comments

--- Week 9/hw9_q1.cpp
+++ Week 9/hw9_q1.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
 #include <vector>
-
+//Initialize wordSplitter function
 void wordSplitter (std::string str);
 const int ALPHABET = 26;
 
 int main() {
-    //Initialize string variables, one for input and one for individual words after I split them apart
+    //Initialize string variable for input
     std::string line;
-        
     //Request user input
     std::cout<<"Please enter a line of text: "<<std::endl;
     getline(std::cin, line);
@@ -18,13 +16,9 @@ int main() {
 }
 
 void wordSplitter (std::string str) {
-   
-    //Initialize counters for the words and letters
-
+    //Initialize array for the letters and counter for words
     int letterCount[ALPHABET] = {};
-    int wordcount = 1;
-
-    
+    int wordcount = 1;    
     //Begin loop to isolate individual letters
     for ( int i = 0 ; i <= str.length() ; i++ ) {
         //Check if the element of the string is a space. If so, increase wordcount        
@@ -36,9 +30,7 @@ void wordSplitter (std::string str) {
             str[i] = str[i] + 32;
         }
     }
-
-    std::cout<<wordcount<<'\t'<<"words"<<std::endl;
-
+    //Begin for loop to find any characters between a-z and the comparable element in the letterCount array
     for ( int i = 0; i < str.length(); i++ ) {
         if ( str[i] >= 'a' && str[i] <= 'z' ) {
             char c = str[i];
@@ -46,13 +38,13 @@ void wordSplitter (std::string str) {
             letterCount[index] = letterCount[index] + 1;
         }
     }
-    // for ( int i = 0; i < ALPHABET; i++ ) {
-    //     std::cout<<letterCount[i];
-    // }
-    // std::cout<<std::endl;
-
+    //Begin output
+    std::cout<<wordcount<<'\t'<<"words"<<std::endl;
+    //Begin loop to output the appropriate values for the indices
     for ( int i = 0; i < ALPHABET; i++ ) {
+        //If the count for any individual letter is 0, ignore it
         if ( letterCount[i] > 0 ) {
+            //Output the value for the index and compare it to the proper letter
             std::cout<<letterCount[i]<<'\t'<<static_cast<char>(i + 'a')<<std::endl;
         }
     }
