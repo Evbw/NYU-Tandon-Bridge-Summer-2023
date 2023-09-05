0391c6149e60be6a6e19eccb3782a88079b35c81
Everett <everett.bowline@gmail.com>
2023-09-04 20:40:39 -0400

    Added getPosNums2 function

--- Week 9/hw9_q3.cpp
+++ Week 9/hw9_q3.cpp
@@ -17,11 +17,11 @@ int main() {
     int* posNumsPtr1;
     int posNumsSize1 = 0;
     int* posNumsPtr2;
-    int posNumsSize2 = 0;
+    int* posNumsSize2 = 0;
     int* posNumsPtr3;
     int posNumsSize3 = 0;
-    int* posNumsPtr4;
-    int posNumsSize4 = 0;
+    int** posNumsPtr4;
+    int* posNumsSize4;
 
     posNumsPtr1 = getPosNums1(arr, ARRAYSIZE, posNumsSize1);
     
@@ -29,11 +29,11 @@ int main() {
     delete [] posNumsPtr1;
     posNumsPtr1 = nullptr;
 
-    // posNumsPtr2 = getPosNums2(arr, ARRAYSIZE, posNumsSize2);
+    posNumsPtr2 = getPosNums2(arr, ARRAYSIZE, posNumsSize2);
     
-    // printArray(posNumsPtr2, posNumsSize2);
-    // delete [] posNumsPtr2;
-    // posNumsPtr2 = nullptr;
+    printArray(posNumsPtr2, *posNumsSize2);
+    delete [] posNumsPtr2;
+    posNumsPtr2 = nullptr;
 
     // getPosNums3(arr, ARRAYSIZE, posNumsSize3);
     
@@ -41,17 +41,14 @@ int main() {
     // delete [] posNumsPtr1;
     // posNumsPtr1 = nullptr;
 
-    // getPosNums4(arr, ARRAYSIZE, posNumsSize4);
-    
-    // printArray(posNumsPtr1, posNumsSize1);
-    // delete [] posNumsPtr1;
-    // posNumsPtr1 = nullptr;
+    getPosNums4(arr, ARRAYSIZE, posNumsPtr4, posNumsSize4);
 }
 
 void printArray(int* arr, int arrSize) {
     for ( int i = 0; i < arrSize; i++ ) {
-        cout<<arr[i]<<endl;
+        cout<<arr[i]<<" ";
     }
+    cout<<endl;
 }
 
 int* getPosNums1(int* arr, int arrSize, int& outPosArrSize) {
@@ -77,8 +74,24 @@ int* getPosNums1(int* arr, int arrSize, int& outPosArrSize) {
 }
 
 int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr) {
+    
+    int j = 0;
 
-    return arr;
+    for ( int i = 0; i < arrSize; i++ ) {
+        if ( arr[i] > 0 ) {
+            *outPosArrSizePtr++;
+        }
+    }
+
+    int *temp = new int[*outPosArrSizePtr];
+
+    for ( int i = 0; i < arrSize; i++ ) {
+        if ( arr[i] > 0 ) {
+            temp[j] = arr[i];
+            j++;
+        }
+    }
+    return temp;
 }
 
 void getPosNums3(int* arr, int arrSize, int*& outPosArr, int& outPosArrSize) {
@@ -87,6 +100,23 @@ void getPosNums3(int* arr, int arrSize, int*& outPosArr, int& outPosArrSize) {
 }
 
 void getPosNums4(int* arr, int arrSize, int** outPosArrPtr, int* outPosArrSizePtr) {
-    outPosArrPtr;
-    arrSize = *outPosArrSizePtr;
+    // outPosArrPtr;
+    // arrSize = *outPosArrSizePtr;
+
+    //    int *outPosArr = new int[arrSize];
+    //    outPosArrPtr = &outPosArr;
+    // *outPosArrPtr = new int [arrSize];
+
+    // int count = 0;
+    // for (int i = 0; i < arrSize; ++i) {
+    //     if (arr[i] > 0){
+    //         (*outPosArrPtr)[count] = arr[i];
+    //         count++;
+    //     }
+    // }
+    // *outPosArrSizePtr = count;
+
+    // printArray(*outPosArrPtr, *outPosArrSizePtr);
+    // delete [] *outPosArrPtr;
+    // *outPosArrPtr = nullptr;
 }
