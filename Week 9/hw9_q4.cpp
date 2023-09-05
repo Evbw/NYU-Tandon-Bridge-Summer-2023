fcc8693b7ae76ca4583b8a0e33968b2477a4ebdf
Everett <everett.bowline@gmail.com>
2023-09-04 21:52:50 -0400

    Built out primary body

---
+++ Week 9/hw9_q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void oddsKeepEvensFlip(int arr[], int arrSize);
+const int ARRAYSIZE = 6;
+
+int main() {
+    int arr[] = {5, 2, 11, 7, 6, 4};
+
+    oddsKeepEvensFlip(arr, ARRAYSIZE);
+}
+
+void oddsKeepEvensFlip(int arr[], int arrSize) {
+    cout<<"Function call test"<<endl;
+}
