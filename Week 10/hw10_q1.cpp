11db7b56ad617c36f49e249e0eec125a9be21a15
Everett <everett.bowline@gmail.com>
2023-09-08 10:43:45 -0400

    Instantiate file and first function

---
+++ Week 10/hw10_q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Declare function
+string* createWordsArray(string sentence, int& outWordsArrSize)
+
+
+int main() {
+   
+}
+
+string* createWordsArray(string sentence, int& outWordsArrSize) {
+
+}
