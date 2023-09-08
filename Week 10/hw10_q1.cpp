34a18e15d6ffd182400563f2a6510bbcc95e33b1
Everett <everett.bowline@gmail.com>
2023-09-08 16:04:48 -0400

    Added comments

--- Week 10/hw10_q1.cpp
+++ Week 10/hw10_q1.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <string>
-
 //Declare functions
 std::string* createWordsArray(std::string sentence, int& outWordsArrSize);
+//Precondition: sentence is a valid string and outWordsArrSize is a valid integer
+//Postcondition: A new array containing individual words will be returned
 void printArray(std::string* arr, int arrSize);
 
 int main() {
+    //Declare variables: An input line, an array size that will be updated, and a dynamic array for strings
     std::string line = "";
     int arrSize = 0;
     std::string* stringarr = new std::string;
-
+    //Request input
     std::cout<<"Please enter a sentence: "<<std::endl;
     getline(std::cin, line);
-
+    //Call the createWordsArray to add the individual words as elements of an array
     stringarr = createWordsArray(line, arrSize);
-
+    //No reason to have an array just hanging around, so we test it by printing
     printArray(stringarr, arrSize);
-    
+    //And then we delete that pointer that's pointing to some random place in memory
     delete [] stringarr;
     stringarr = nullptr;
 }
-
+//Simple function to print an array
 void printArray(std::string* arr, int arrSize) {
     for ( int i = 0; i < arrSize; i++ ) {
         std::cout<<arr[i]<<" ";
@@ -29,31 +31,31 @@ void printArray(std::string* arr, int arrSize) {
 }
 
 std::string* createWordsArray(std::string sentence, int& outWordsArrSize) {
+    //Declare a variable for separating the words and counting the individual numbers of words
     std::string word;
     int wordcounter = 0;
-
-
+    //Begin loop to count the number of words in the string
     for ( int i = 0; i <= ( sentence.length() ); i++ ) {
         if ( sentence[i] == ' ' || i == sentence.length() ) {
             wordcounter++;
         }
     }
-
+    //Create new temporary dynamyic array the size of the nmber of words
     std::string *temp = new std::string[wordcounter];
+    //Set the call-by-reference array size to the count
     outWordsArrSize = wordcounter;
-
+    //Declare variable for properly tracking words. Reset wordcounter
     int j = 0;
-    int w = 0;
+    wordcounter = 0;
+    //Begin loop to separate words and add them as elements to an array
     for ( int i = 0; i <= ( sentence.length() ); i++ ) {
-        // std::cout<<i<<std::endl;
+        //Conditional to delimit words by a space or the end of the string.
         if ( sentence[i] == ' ' || i == sentence.length() ) {
-            // std::cout<<sentence.length()<<std::endl;
-            // std::cout<<sentence.substr(i, (i-j))<<std::endl;
-            temp[w] = sentence.substr(j, (i-j));
+            temp[wordcounter] = sentence.substr(j, (i-j));
             j = ( i + 1 );
-            w++;
+            wordcounter++;
         }
     }
-    
+    //Return the temporary array
     return temp;
 }
