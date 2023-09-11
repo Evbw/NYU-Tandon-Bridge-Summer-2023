ffb203959d607d94a03112bd8aa215f30ecb529d
Everett <everett.bowline@gmail.com>
2023-09-11 13:09:17 -0400

    Began building out main1 function

---
+++ Week 10/hw10_q3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main1();
+int main2();
+void printArray(int* arr, int arrSize);
+
+const int ARRAYSIZE = 10;
+
+int main() {
+
+    cout<<"Non-vector version: "<<endl;
+    main1();
+
+    // cout<<"Vector version: "<<endl;
+    // main2();
+}
+
+int main1() {
+    int n, searchn;
+    int* arr = new int[ARRAYSIZE]();
+    int* searchArr = new int[ARRAYSIZE]();
+
+    cout<<"Please enter a sequence of positive integers, each in a separate line."<<endl;
+    cout<<"End you input by typing -1."<<endl;
+    cin>>n;
+
+    int i = 0;
+    while ( n > -1 ) {
+        arr[i] = n;
+        cin>>n;
+        i++;
+    }
+
+    cout<<"Please enter a number you want to search."<<endl;
+    cin>>searchn;
+
+    int j = 0;
+    for ( int i = 0; i < j; i++ ) {
+        if ( searchn == arr[i] ) {
+            searchArr[j] = i + 1;
+            j++;
+        }
+    }
+
+    printArray(searchArr, j);
+}
+
+void printArray(int* arr, int arrSize) {
+    for ( int i = 0; i < arrSize; i++ ) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
