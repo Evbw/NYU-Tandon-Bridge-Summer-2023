f5766fe09ce2a88c444641e538c59f346e8231ad
Everett <everett.bowline@gmail.com>
2023-09-17 14:52:53 -0400

    Finished printOppositeTriangles function

--- Week 11/hw11_q1.cpp
+++ Week 11/hw11_q1.cpp
@@ -35,8 +35,8 @@ void printTriangle (int n) {
 
 void printOppositeTriangles (int n) {
    
-    if ( n == 1 ) {
-        cout<<"*"<<endl;
+    if ( n == 0 ) {
+        return;
     }
     else {
         for ( int i = 0; i < n; i++ ) {
@@ -44,6 +44,10 @@ void printOppositeTriangles (int n) {
         }
         cout<<endl;
         printOppositeTriangles(n - 1);
+        for ( int i = 0; i < n; i++ ) {
+            cout<<"*";
+        }
+        cout<<endl;
     }
 }
 
