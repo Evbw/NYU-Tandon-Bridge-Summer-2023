7b71f413c8e353df1e8b2892076260bba67aba05
Everett <everett.bowline@gmail.com>
2023-09-22 14:37:29 -0400

    Added comments

--- Week 11/hw11_q4.cpp
+++ Week 11/hw11_q4.cpp
@@ -1,35 +1,44 @@
 #include <iostream>
 using namespace std;
-
+//Initialize function
 int jumpIt (int arr[], int arrSize);
+//Precondition: arr is filled with valid integers of arrSize
+//Postcondition: The sums of the shortest path, moving forward 1 or 2 steps from index 0, will be returned
 
+//Declare constants for array sizes
 const int ARRSIZE = 6;
 const int ARRSIZE2 = 9;
 
 int main() {
+    //Declare arrays
     int arr[ARRSIZE] = {0, 3, 80, 6, 57, 10};
     int arr2[ARRSIZE2] = {0, 3, 80, 100, 100, 100, 6, 57, 10};
+    //Declare a variable to handle the sum of the function
     int n;
-
+    //Call the function with two array sizes and print the result
     n = jumpIt (arr, ARRSIZE);
-    cout<<n<<endl;
+    cout<<"The lowest total cost for the first set is "<<n<<endl;
     n = jumpIt (arr2, ARRSIZE2);
-    cout<<n<<endl;
+    cout<<"The lowest total cost for the second set is "<<n<<endl;
 }
 
 int jumpIt (int arr[], int arrSize) {
-    int jump = 0;
-
+    //Declare a return variable for the summation of each move
+    int jumps = 0;
+    //Base case. If the arrSize is 1 or less, return the final element of the array
     if ( arrSize <= 1 ) {
         return arr[0];
     }
+    //Otherwise, we compare the sizes of the moves and take the one with the lowest sum.
     else {
         if ( (arr[arrSize - 1] + arr[arrSize - 2]) < (arr[arrSize - 1] + arr[arrSize - 3])) {
-            jump = arr[arrSize - 1] + jumpIt(arr, arrSize - 1);
+            //And add that to the return variable
+            jumps = arr[arrSize - 1] + jumpIt(arr, arrSize - 1);
         }
         else {
-            jump = arr[arrSize - 1] + jumpIt(arr, arrSize - 2);
+            jumps = arr[arrSize - 1] + jumpIt(arr, arrSize - 2);
         }
     }
-    return jump;
+    //And now we return the value
+    return jumps;
 }
