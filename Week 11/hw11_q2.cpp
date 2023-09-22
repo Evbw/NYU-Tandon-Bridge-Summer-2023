edab83a41d7186b876d27fa1b14485e625bbfd92
Everett <everett.bowline@gmail.com>
2023-09-22 12:54:06 -0400

    Built out isSorted function

--- Week 11/hw11_q2.cpp
+++ Week 11/hw11_q2.cpp
@@ -2,13 +2,32 @@
 using namespace std;
 
 int sumOfSquares(int arr[], int arrSize);
-// bool isSorted(int arr[], int arrSize);
+bool isSorted(int arr[], int arrSize);
 
 int main() {
     int arr[4] = {2, -1, 3, 10};
+    int arr2[8] = {-200, -150, -43, 3, 10, 11, 11, 84};
     int sum = 0;
+    
     sum = sumOfSquares(arr, 4);
     cout<<sum<<endl;
+
+    sum = sumOfSquares(arr2, 8);
+    cout<<sum<<endl;
+
+    if (isSorted(arr, 4)) {
+        cout<<"good job"<<endl;
+    }
+    else {
+        cout<<"aww beans"<<endl;
+    }
+
+    if (isSorted(arr2, 8)) {
+        cout<<"good job"<<endl;
+    }
+    else {
+        cout<<"aww beans"<<endl;
+    }
 
 }
 
@@ -25,6 +44,18 @@ int sumOfSquares(int arr[], int arrSize) {
     return sum;
 }
 
-// bool isSorted(int arr[], int arrSize) {
-    
-// }
+bool isSorted(int arr[], int arrSize) {
+    bool truthiness;
+
+    if ( arrSize == 1 ){
+        return true;
+    }
+    else {
+        if ( arr[arrSize-1] < arr[arrSize-2] ) {
+            return false;
+        }
+        truthiness = isSorted(arr, arrSize - 1);
+    }
+
+    return truthiness;
+}
