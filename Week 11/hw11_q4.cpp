a75a85ac1fcff709e2149081f2c4ecfbfdb08b93
Everett <everett.bowline@gmail.com>
2023-09-22 14:26:38 -0400

    Finished jumpIt algorithm

--- Week 11/hw11_q4.cpp
+++ Week 11/hw11_q4.cpp
@@ -4,33 +4,31 @@ using namespace std;
 int jumpIt (int arr[], int arrSize);
 
 const int ARRSIZE = 6;
+const int ARRSIZE2 = 9;
 
 int main() {
     int arr[ARRSIZE] = {0, 3, 80, 6, 57, 10};
+    int arr2[ARRSIZE2] = {0, 3, 80, 100, 100, 100, 6, 57, 10};
     int n;
 
     n = jumpIt (arr, ARRSIZE);
-
+    cout<<n<<endl;
+    n = jumpIt (arr2, ARRSIZE2);
     cout<<n<<endl;
 }
 
 int jumpIt (int arr[], int arrSize) {
     int jump = 0;
 
-    if ( arrSize <= 2 ) {
-        cout<<"arrSize "<<arrSize<<endl;
+    if ( arrSize <= 1 ) {
         return arr[0];
     }
     else {
         if ( (arr[arrSize - 1] + arr[arrSize - 2]) < (arr[arrSize - 1] + arr[arrSize - 3])) {
-            // jump = arr[arrSize - 1] + arr[arrSize - 2];
-            jump = arr[arrSize - 1] + jumpIt(arr, arrSize - 2);
-            cout<<"jumpif "<<jump<<endl;
+            jump = arr[arrSize - 1] + jumpIt(arr, arrSize - 1);
         }
         else {
-            // jump = arr[arrSize - 1] + arr[arrSize - 3];
-            jump = arr[arrSize - 1] + jumpIt(arr, arrSize - 3);
-            cout<<"jumpelse "<<jump<<endl;
+            jump = arr[arrSize - 1] + jumpIt(arr, arrSize - 2);
         }
     }
     return jump;
