96493b520393c2eb9ab1c04946a30730736e923f
Everett <everett.bowline@gmail.com>
2023-09-22 13:24:47 -0400

    Added test suite for main function

---
+++ Week 11/hw11_q3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int minInArray1 (int arr[], int arrSize);
+int minInArray2 (int arr[], int low, int high);
+
+int main() {
+    int arr[10] = {9, -2, 14, 12, 3, 6, 2, 1, -9, 15};
+    int res1, res2, res3, res4;
+
+    // res1 = minInArray1(arr, 10);
+    // res2 = minInArray2(arr, 0, 9);
+    cout<<res1<<" "<<res2<<endl; //should both be -9
+
+    // res3 = minInArray2(arr, 2, 5);
+    // res4 = minInArray1(arr+2, 4); //arr+2 is equivalent to &(arr[2])
+    cout<<res3<<" "<<res4<<endl; //should both be 3
+
+    return 0;
+}
+
+// int minInArray1 (int arr[], int arrSize) {
+    
+// }
+
+// int minInArray2 (int arr[], int low, int high) {
+    
+// }
