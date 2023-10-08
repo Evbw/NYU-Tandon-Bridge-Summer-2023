395c0d513489f6bc10869118ab1005a2e3091c13
Everett <everett.bowline@gmail.com>
2023-10-08 13:18:25 -0400

    Added Ant and Doodlebug classes

--- Week 13/hw13.cpp
+++ Week 13/hw13.cpp
@@ -12,6 +12,31 @@ class Organism {
 
     public:
         Organism(const Grid& position);
+        Organism();
+        virtual void move();
+        virtual void breed();
+        virtual void die();
+};
+
+class Ant {
+    private:
+        int breed;
+        int starve;
+    public:
+        Ant(const Grid& position);
+        Ant();
+        virtual void move();
+        virtual void breed();
+        virtual void die();
+};
+
+class Doodlebug {
+    private:
+        int breed;
+        int starve;
+    public:
+        Doodlebug(const Grid& position);
+        Doodlebug();
         virtual void move();
         virtual void breed();
         virtual void die();
