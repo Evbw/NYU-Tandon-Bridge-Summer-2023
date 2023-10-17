217e8bb9a97f470d1acccdc8aa09e18dca21b504
Everett <everett.bowline@gmail.com>
2023-10-16 22:33:07 -0400

    Finalized algorithm

--- Week 14/hw14.cpp
+++ Week 14/hw14.cpp
@@ -6,8 +6,8 @@ vector<int> minMax(vector<int>& v);
 vector<int> minMax(vector<int>& v, int start, int end);
 
 int main() {
-    vector<int> testVector = {9, 0, 8, 1, 7, 2, 6, 3, 5, 4};
-    
+    vector<int> testVector = {9, 0, 8, 1, 7, -50, 2, 6, 4};
+        
     if (testVector.size() == 0) {
         cout<<"Vector is empty. No result.";
         return 0;
@@ -15,10 +15,11 @@ int main() {
 
     vector<int> minMaxResult = minMax(testVector);
     
-    for (int i = 0; i < minMaxResult.size() - 1; i++) {
-        cout<<minMaxResult[i]<<" "<<endl;
+    for (int i = 0; i < minMaxResult.size(); i++) {
+        cout<<minMaxResult[i]<<" ";
     }
-
+    cout<<endl;
+    return 0;
 }
 
 vector<int> minMax(vector<int>& v) {
@@ -27,18 +28,30 @@ vector<int> minMax(vector<int>& v) {
 
 vector<int> minMax(vector<int>& v, int start, int end) {
     vector<int> returnVector;
+    vector<int> leftVector;
+    vector<int> rightVector;
 
-    if (start >= end) {
+    if (start == end) {
         returnVector.push_back(v[start]);
-        for (int i = 0; i < returnVector.size() - 1; i++) {
-            cout<<returnVector[i]<<" "<<endl;
-        }
+        returnVector.push_back(v[start]);    
         return returnVector;
     }
     else {
         int mid = (start + end)/2;
-        minMax(v, 0, mid);
-        minMax(v, mid+1, end);
+        leftVector = minMax(v, 0, mid);
+        rightVector = minMax(v, mid+1, end);
+        if ( leftVector[0] <= rightVector[0] ) {
+            returnVector.push_back(leftVector[0]);
+        }
+        else {
+            returnVector.push_back(rightVector[0]);
+        }
+        if ( leftVector[1] >= rightVector[1] ) {
+            returnVector.push_back(leftVector[1]);
+        }
+        else {
+            returnVector.push_back(rightVector[1]);
+        }
     }
     
     return returnVector;
