18fbc93b8c7a545930ffb96211bda1f4a975b0bd
Everett <everett.bowline@gmail.com>
2023-10-20 12:20:36 -0400

    Wrote payBack and openInputFile functions

--- Week 15/hw15.cpp
+++ Week 15/hw15.cpp
@@ -13,9 +13,55 @@ class Diner {
         Diner() {};
         double getAmountPaid() {return amountPaid;}
         string getName() {return name;}
-        void setAmountPaid(double amountPaid)
-        void setName(string name)
+        void setAmountPaid(double newAmountPaid) { amountPaid = newAmountPaid;}
+        void setName(string newName) { name = newName; }
+
+        friend void payBack(Diner& loanShark, Diner& debtor, double groupAverage) {
+            double amountOwed;
+            double amountOwing;
+            double amountStillOwed;
+            
+            amountOwing = debtor.amountPaid - groupAverage;
+            amountOwed = loanShark.amountPaid - groupAverage;
+
+            if( amountOwing > amountOwed ) {
+                amountOwing -= amountOwed;
+                loanShark.amountPaid -= amountOwed;
+                debtor.amountPaid = amountOwing;
+                if (loanShark.amountPaid == groupAverage && debtor.amountPaid != groupAverage) {
+                    cout<<loanShark.name<<" is paid back! ";
+                    cout<<debtor.name<<" still owes $"<<amountOwing<<endl;
+                }
+                else if (loanShark.amountPaid == groupAverage && debtor.amountPaid == groupAverage) {
+                    cout<<loanShark.name<<" is paid back! ";
+                    cout<<debtor.name<<" is paid up!"<<endl;
+                }
+            }
+            else {
+                loanShark.amountPaid -= amountOwing;
+                debtor.amountPaid += amountOwing;
+                amountStillOwed = loanShark.amountPaid - groupAverage;
+                if (debtor.amountPaid == groupAverage && loanShark.amountPaid != groupAverage) {
+                    cout<<debtor.name<<" is paid up! ";
+                    cout<<loanShark.name<<" is still owed $"<<amountStillOwed<<endl;
+                }
+            }
+        }
 };
+
+void openInputFile(ifstream& inFile) {
+    string filename;
+    cout<<"Enter the filename: "<<endl;
+    cin>>filename;
+    inFile.open(filename);
+    while (!inFile) {
+        cout<<"File failed to open."<<endl;
+        out<<"Enter the filename: "<<endl;
+        cin>>filename;
+        inFile.clear();
+        inFile.open(filename);
+    }
+}
 
 int main() {
     
