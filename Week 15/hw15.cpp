938bc62873ef6bf7dd86c137604235e3742d0e88
Everett <everett.bowline@gmail.com>
2023-10-22 10:53:50 -0400

    Copied Linked List functions and changed Diner vector to a linked list

--- Week 15/hw15.cpp
+++ Week 15/hw15.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <list>
 using namespace std;
 
+template <class T>
 class Diner {
     private:
-        double amountPaid;
-        string name;
+        T amountPaid;
+        T name;
     public:
         Diner(double amountPaid, string name) : amountPaid(amountPaid), name(name) {};
         Diner() {};
@@ -49,6 +51,91 @@ class Diner {
         }
 };
 
+template <class T>
+class LList;
+
+template <class T>
+class LListNode {
+private:
+	T data;
+	LListNode<T>* next;
+	LListNode<T>* prev;
+public:
+	LListNode() : next(nullptr), prev(nullptr) {}
+	LListNode(const T& newdata = T(), LListNode<T>* newnext = nullptr, LListNode<T>* newprev = nullptr) :data(newdata), next(newnext), prev(newprev) {}
+	friend class LList<T>;
+};
+
+template <class T>
+class LList {
+	LListNode<T>* head;
+	LListNode<T>* tail;
+	
+public:
+	LList();
+	~LList();
+	LList(const LList<T>& rhs);
+	LList<T>& operator=(const LList<T>& rhs);
+
+	void clear() { while (!isEmpty()) pop_front(); }
+	void push_back(const T& data) { tail->prev = new LListNode(data, tail, tail->prev); tail->prev->prev->next = tail->prev; }
+	void push_front(const T& data) { head->next = new LListNode(data, head->next, head);  head->next->next->prev = head->next; }
+	T pop_back();
+	T pop_front();
+	int size();
+	bool isEmpty() { return head->next == tail; }
+};
+template <class T>
+LList<T>& LList<T>::operator=(const LList<T>& rhs) {
+	if (this == &rhs)
+		return *this;
+	clear();
+	LListNode<T>* ptr = rhs.head->next;
+	while (ptr != rhs.tail) {
+		push_back(ptr->data);
+	}
+	return *this;
+}
+template <class T>
+int LList<T>::size() {
+	LListNode<T>* ptr = head->next;
+	int count = 0;
+	while (ptr != tail) {
+		count++;
+		ptr = ptr->next;
+	}
+	return count;
+}
+template <class T>
+T LList<T>::pop_back() {
+	if (tail->prev == head) //empty list!
+		return T(); //not sure what to do here?  User did something dumb.
+	T retval = tail->prev->data;
+	LListNode<T>* nodeToDelete = tail->prev;
+	tail->prev = nodeToDelete->prev;
+	nodeToDelete->prev->next = nodeToDelete->next;
+	delete nodeToDelete;
+	return retval;
+}
+template <class T>
+T LList<T>::pop_front() {
+	if (tail->prev == head) //empty list!
+		return T(); //not sure what to do here?  User did something dumb.
+	T retval = head->next->data;
+	LListNode<T>* nodeToDelete = head->next;
+	head->next = nodeToDelete->next;
+	head->next->prev = head;
+	delete nodeToDelete;
+	return retval;
+}
+template <class T>
+LList<T>::~LList() {
+	clear();
+	delete head;
+	delete tail;
+}
+
+
 void openInputFile(ifstream& inFile) {
     string filename;
     cout<<"Enter the filename: "<<endl;
@@ -56,7 +143,7 @@ void openInputFile(ifstream& inFile) {
     inFile.open(filename);
     while (!inFile) {
         cout<<"File failed to open."<<endl;
-        out<<"Enter the filename: "<<endl;
+        cout<<"Enter the filename: "<<endl;
         cin>>filename;
         inFile.clear();
         inFile.open(filename);
@@ -64,5 +151,9 @@ void openInputFile(ifstream& inFile) {
 }
 
 int main() {
+    ifstream inFile;
+    openInputFile(inFile);
+    vector<int> dinerVector;
     
+
 }
