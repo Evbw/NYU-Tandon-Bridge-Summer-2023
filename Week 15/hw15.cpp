dda4a0072b6331778ad236df276228785dba853f
Everett <everett.bowline@gmail.com>
2023-10-24 15:12:24 -0400

    Updated List and Node functions

--- Week 15/hw15.cpp
+++ Week 15/hw15.cpp
@@ -23,12 +23,18 @@ class Diner {
     public:   
         Diner(double amountPaid, string name) : amountPaid(amountPaid), name(name) {};
         Diner() {};
-        double getAmountPaid() {return amountPaid;}
-        string getName() {return name;}
+        double getAmountPaid() const {return amountPaid;}
+        string getName() const {return name;}
         void setAmountPaid(double newAmountPaid) { amountPaid = newAmountPaid;}
         void setName(string newName) { name = newName; }
         friend void payBack(Diner& loanShark, Diner& debtor, double groupAverage);
+        friend ostream& operator<<(ostream& outs, const Diner& diner);        
 };
+
+ostream& operator<<(ostream& outs, const Diner& diner) {
+    outs<<diner.getName()<<" has paid $"<<diner.getAmountPaid()<<"."<<endl;
+    return outs;
+}
 
 template<class T>
 class LList{
@@ -46,6 +52,7 @@ public:
         this->tail=nullptr;
         this->size=0;
     }
+    ~LList();
     void addend(node<T>* end){
         if(this->size==0){this->head=end;}
         this->tail->next=end;
@@ -77,44 +84,63 @@ public:
         this->size--;
     }
     void print(){
-        if(this->size==0){cout<<"List is empty"<<endl;return;}
+        if(this->size==0){
+            cout<<"List is empty"<<endl;
+            return;
+        }
         node<T>* on=this->head;
         while(on!=nullptr){
-            cout<<on->val<<" ";
+            cout<<on->val;
             on=on->next;
         }
         cout<<endl;
     }
 };
 
+template <class T>
+LList<T>::~LList() {
+	while(head != nullptr) {
+        node<T>* temp = head;
+        head = head->next;
+        delete temp;
+    }
+    tail = nullptr;
+}
+
 void payBack(Diner& loanShark, Diner& debtor, double groupAverage) {
     double amountOwed;
     double amountOwing;
     double amountStillOwed;
+    double newAmountPaid;
+    double newAmountPaidBack;
     
-    amountOwing = debtor.amountPaid - groupAverage;
-    amountOwed = loanShark.amountPaid - groupAverage;
+    amountOwing = debtor.getAmountPaid() - groupAverage;
+    amountOwed = loanShark.getAmountPaid() - groupAverage;
 
     if( amountOwing > amountOwed ) {
         amountOwing -= amountOwed;
-        loanShark.amountPaid -= amountOwed;
-        debtor.amountPaid = amountOwing;
-        if (loanShark.amountPaid == groupAverage && debtor.amountPaid != groupAverage) {
-            cout<<loanShark.name<<" is paid back! ";
-            cout<<debtor.name<<" still owes $"<<amountOwing<<endl;
+        newAmountPaid = loanShark.getAmountPaid() - amountOwed;
+        loanShark.setAmountPaid(newAmountPaid);
+        newAmountPaidBack = debtor.getAmountPaid() + amountOwing;
+        debtor.setAmountPaid(newAmountPaidBack);
+        if (loanShark.getAmountPaid() == groupAverage && debtor.getAmountPaid() != groupAverage) {
+            cout<<loanShark.getName()<<" is paid back! ";
+            cout<<debtor.getName()<<" still owes $"<<amountOwing<<endl;
         }
-        else if (loanShark.amountPaid == groupAverage && debtor.amountPaid == groupAverage) {
-            cout<<loanShark.name<<" is paid back! ";
-            cout<<debtor.name<<" is paid up!"<<endl;
+        else if (loanShark.getAmountPaid() == groupAverage && debtor.getAmountPaid() == groupAverage) {
+            cout<<loanShark.getName()<<" is paid back! ";
+            cout<<debtor.getName()<<" is paid up!"<<endl;
         }
     }
     else {
-        loanShark.amountPaid -= amountOwing;
-        debtor.amountPaid += amountOwing;
-        amountStillOwed = loanShark.amountPaid - groupAverage;
-        if (debtor.amountPaid == groupAverage && loanShark.amountPaid != groupAverage) {
-            cout<<debtor.name<<" is paid up! ";
-            cout<<loanShark.name<<" is still owed $"<<amountStillOwed<<endl;
+        newAmountPaid = loanShark.getAmountPaid() - amountOwing;
+        loanShark.setAmountPaid(newAmountPaid);
+        newAmountPaidBack = debtor.getAmountPaid() + amountOwing;
+        debtor.setAmountPaid(newAmountPaidBack);
+        amountStillOwed = loanShark.getAmountPaid() - groupAverage;
+        if (debtor.getAmountPaid() == groupAverage && loanShark.getAmountPaid() != groupAverage) {
+            cout<<debtor.getName()<<" is paid up! ";
+            cout<<loanShark.getName()<<" is still owed $"<<amountStillOwed<<endl;
         }
     }
 }
@@ -145,6 +171,16 @@ int main() {
         if ( inSpace != string::npos ) {
             amount = stod(input.substr(0, inSpace));
             name = input.substr(inSpace + 1);
+            Diner d(amount, name);
+            node<Diner>* newDinerNode = new node<Diner>(d);
+            if(dinerList.size == 0) {
+                dinerList.head = newDinerNode;
+                dinerList.tail = newDinerNode;
+                dinerList.size++;
+            }
+            else {
+                dinerList.addend(newDinerNode);
+            }
         }
         else {
             cout<<"Invalid line"<<endl;
@@ -152,7 +188,7 @@ int main() {
         }
     }
     inFile.close();
+    dinerList.print();
 
 return 0;    
-
 }
