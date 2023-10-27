7686458f1cbd41d5162c6e6ac5a17a63e6899c30
Everett <everett.bowline@gmail.com>
2023-10-27 16:39:08 -0400

    Updated output text

--- Week 16/hw16.cpp
+++ Week 16/hw16.cpp
@@ -86,7 +86,7 @@ int main() {
         cout<<"The file is balanced!"<<endl;
     }
     else {
-        cout<<"Did you ever hear the Tragedy of Darth Plagueis the Wise? - No. - I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."<<endl;
+        cout<<"The file is not balanced!"<<endl;
     }
     return 0;
 }
