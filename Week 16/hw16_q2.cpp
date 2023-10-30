4b78ecf208fe664b9d1aeb6f84f97baa5c3e9278
Everett <everett.bowline@gmail.com>
2023-10-30 09:52:33 -0400

    Add class member variables and functions

--- Week 16/hw16_q2.cpp
+++ Week 16/hw16_q2.cpp
@@ -3,10 +3,33 @@
 using namespace std;
 
 class VectorQueue {
-
+    private:
+        int start;
+        int size;
+        vector<int> vQueue;
+    public:
+        VectorQueue() : start(0), size(0) {};
+        void vectorPush (int value) {
+            vQueue.push_back(value);
+            size++;
+        }
+        void vectorPop () {
+            int index;
+            vQueue[index++];
+        }
+        bool empty() {
+            size = 0;
+            return size;
+        }
 };
 
 int main() {
-    
+    VectorQueue v;
+
+    for ( int i = 0; i < 10; i++ ) {
+        v.vectorPush(i);
+        cout<<v<<" ";
+    }
+
     return 0;
 }
