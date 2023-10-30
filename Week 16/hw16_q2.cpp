9088bd9d56a099ac22bfd949685cd20f248d9061
Everett <everett.bowline@gmail.com>
2023-10-30 10:32:11 -0400

    Added overloaded operator functions to test output

--- Week 16/hw16_q2.cpp
+++ Week 16/hw16_q2.cpp
@@ -5,30 +5,59 @@ using namespace std;
 class VectorQueue {
     private:
         int start;
-        int size;
+        int end;
         vector<int> vQueue;
     public:
-        VectorQueue() : start(0), size(0) {};
+        VectorQueue() : start(0), end(0) {};
         void vectorPush (int value) {
             vQueue.push_back(value);
-            size++;
+            end++;
         }
         void vectorPop () {
-            int index;
-            vQueue[index++];
+            if ( !empty() ) {
+                start++;
+                if ( start == end ) {
+                    vQueue.clear();
+                    start = end = 0;
+                }
+            }
         }
         bool empty() {
-            size = 0;
-            return size;
+            return start == end;
         }
+        int size() const {
+            return end - start;
+        }
+        int operator[]( int index ) const {
+            if ( index < size() ) {
+                return vQueue[start + index];
+            }
+            if ( index < start || index > size() ) {
+                cout<<"Out of range"<<endl;
+            }
+        }
+
+        friend ostream& operator<<( ostream& outs, const VectorQueue& vq );
 };
+
+ostream& operator<<( ostream& outs, const VectorQueue& vq ) {
+    for ( int i = 0; i < vq.size(); i++ ) {
+        outs<<vq[i]<<" ";
+    }
+    return outs;
+}
 
 int main() {
     VectorQueue vq;
 
     for ( int i = 0; i < 10; i++ ) {
         vq.vectorPush(i);
-        cout<<vq<<" ";
+        cout<<vq<<endl;
+    }
+
+    for ( int i = 0; i < 10; i++ ) {
+        vq.vectorPop();
+        cout<<vq<<endl;
     }
 
     return 0;
