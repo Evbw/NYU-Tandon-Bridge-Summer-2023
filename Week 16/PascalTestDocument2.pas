c91bcc3aeb6ede62e79d9681664981571ad516d9
Everett <everett.bowline@gmail.com>
2024-01-11 09:43:56 -0500

    Adding Week 16

---
+++ Week 16/PascalTestDocument2.pas
@@ -0,0 +1,23 @@
+begin
+    x := 5;
+    if (x > 3) then
+    begin
+        y := x + 10;
+        while (y > 0) do
+        begin
+            if (y mod 2 = 0) then
+            begin
+                ([{}]);
+            end
+            else
+            begin
+                ({[()]});
+            end
+            y := y - 1;
+        end
+    end
+    else
+    begin
+        ();
+    end
+en
